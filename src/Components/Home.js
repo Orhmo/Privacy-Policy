8fafd19c9b97ed69d93e553117c88635caf4e4f7
Valentina <98624335+Orhmo@users.noreply.github.com>
2022-11-17 05:29:01 +0100

    Home Component

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="Landing">
+
+      <div className="hero bg-[#ecfeff] w-[100%] h-[50vh] pt-44">
+      <h2 className="flex text-[#418299] text-[48px] font-[700] head align-center justify-center">Privacy Policy</h2>
+      <h3 className="flex text-[#393939] text-[24px] font-[700] head align-center justify-center">If you can't find answers for your questions?
+      <span className="flex text-[#418299] text-[24px] font-[700] head"> Contact Us</span>
+      </h3>
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
